d4b71b9ada186b86dd5a0316f3d4b47cdca2227d
GezartKraja <gkraja2003@gmail.com>
2024-05-26 11:21:37 +0200

    add navbar to the project

--- store/views.py
+++ store/views.py
@@ -1,11 +1,27 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
+
+
+def category(request, foo):
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category_name': category.name})
+    except:
+        messages.error(request, 'Category does not exist')
+        return redirect('home')
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
 
 
 def home(request):
