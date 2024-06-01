5941c1504436285c9f7f856df50c0e2c63f9d167
GezartKraja <gkraja2003@gmail.com>
2024-06-01 11:40:27 +0200

    Your commit message here

--- store/forms.py
+++ store/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",
                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
